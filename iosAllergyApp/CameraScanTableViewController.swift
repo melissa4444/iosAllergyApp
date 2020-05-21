414ead854816b28810d02f0a18f18345e52ec436
mestrada7@mail.sfsu.edu <44541773+melissa4444@users.noreply.github.com>
2020-05-20 17:06:33 -0700

    update going to try and set up the camera scan now

---
+++ iosAllergyApp/CameraScanTableViewController.swift
@@ -0,0 +1,31 @@
+//
+//  CameraScanTableViewController.swift
+//  iosAllergyApp
+//
+//  Created by Melissa Nguyen on 5/20/20.
+//  Copyright © 2020 Melissa Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class CameraScanTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+}
--- iosAllergyApp/View Controllers/HouseViewController.swift
+++ iosAllergyApp/View Controllers/HouseViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class HouseViewController: UIViewController {
 
   
+
     
     
     
